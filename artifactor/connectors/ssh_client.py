7cb8d4902517a7e89aa791a068bdf46b5027f3a7
3d6564 <12701186+3d6564@users.noreply.github.com>
2024-08-01 16:40:06 -0500

    remove hosts if bad authentication, fix outputs (#15)

--- artifactor/connectors/ssh_client.py
+++ artifactor/connectors/ssh_client.py
@@ -154,9 +154,9 @@ class SSHClient:
                                                              target_password,
                                                              sudo)
                 if error:
-                    print(f'\033[1;31mHost {host} error: {error}\033[0m')
+                    #print(f'\033[1;31mHost {host} error: {error}\033[0m')
                     output = error
             return host, output
         except Exception as e:
-            print(f"\033[1;31mHost {host} error: {e}\033[0m")
-            return host, "Error: " + str(e)
+            #print(f"\033[1;31mHost {host} error: {e}\033[0m")
+            return host, "error: " + str(e)
